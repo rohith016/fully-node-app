f16c7907b276eb48adc73712b7b2d5c8fa5a76ec
Rohith Ramachandran <rohithdpr016@gmail.com>
2023-07-25 15:51:58 +0530

    add test cases

--- app.js
+++ app.js
@@ -50,10 +50,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is running on port ${process.env.PORT}`);
+// });
 
 module.exports = app; // for testing
---
+++ test/user.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('User Endpoints', () => {
+  it('should create a new user', async () => {
+    const res = await request(app)
+      .post('/users')
+      .send({
+        firstName: 'Test',
+        lastName: 'User',
+        email: 'test@user.com',
+        password: 'test123'
+      });
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty('email', 'test@user.com');
+  });
+});
