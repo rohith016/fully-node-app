d309b047d5ef1ba3b94391ab4e43f1d7132d9dcd
Rohith Ramachandran <rohithdpr016@gmail.com>
2023-07-25 15:36:20 +0530

    add api rate limiter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config(); // Load environment variables
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const routes = require('./src/routes');
 const connectDB = require('./src/config/db');
 
@@ -8,9 +9,24 @@ const app = express();
 // connect to the db
 connectDB();
 
+// Enable reverse proxy support in Express. This causes the
+// the "X-Forwarded-Proto" header field to be trusted so its
+// value can be used to determine the protocol. See 
+// http://expressjs.com/api#app-settings for more details.
+app.set('trust proxy', 1);
+
+
 // Middleware Setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Apply rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
 
 // Routes
 app.use('/', routes);
