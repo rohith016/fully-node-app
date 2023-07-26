49b8f0e6faa1ad557671121e36635bb417ee1cc3
Rohith Ramachandran <rohithdpr016@gmail.com>
2023-07-26 17:32:18 +0530

    add common response message to reuse

---
+++ src/config/messages.js
@@ -0,0 +1,15 @@
+// config/messages.js
+
+module.exports = {
+    GENERIC_ERROR: 'An error occurred',
+    USER_NOT_FOUND: 'User not found',
+    USER_RETRIEVE : "User retrieved successfully",
+    USERS_RETRIEVE : "Users retrieved successfully",
+    USER_CREATED_SUCCESS: 'User Created Successfully',
+    USER_UPDATED_SUCCESS: 'User Updated Successfully',
+    USER_DELETED_SUCCESS: 'User Deleted Successfully',
+    EMAIL_EXIST: "Email Already Exists",
+    LOGIN_SUCCESS : "Users Logged Successfully"
+    // ... other messages ...
+  };
+  
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@ const userService = require('../services/userService');
 const AppError = require('../utils/AppError');
 const { successResponse, errorResponse } = require('../utils/response');
 const HTTP = require('../config/httpCodes');
+const MESSAGE = require('../config/messages');
 /**
  * List all users
  * @param {*} req 
@@ -11,10 +12,10 @@ exports.list = async (req, res) => {
   try {
     const users = await userService.userList(req.body);
     // res.json(users);
-    return res.status(HTTP.STATUS_OK).json(successResponse('Users retrieved successfully', users, HTTP.STATUS_OK));
+    return res.status(HTTP.STATUS_OK).json(successResponse(MESSAGE.USERS_RETRIEVE, users, HTTP.STATUS_OK));
   } catch (error) {
     // res.status(500).end(err)
-    return res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse('An error occurred', err, HTTP.STATUS_SERVER_ERROR));
+    return res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse(MESSAGE.GENERIC_ERROR, err, HTTP.STATUS_SERVER_ERROR));
   }
   
 };
@@ -28,12 +29,12 @@ exports.show = async (req, res) => {
   try {
     const user = await userService.getUserById(req.params.id);
     if (user) {
-      return res.status(HTTP.STATUS_OK).json(successResponse('User retrieved successfully', user, HTTP.STATUS_OK));
+      return res.status(HTTP.STATUS_OK).json(successResponse(MESSAGE.USER_RETRIEVE, user, HTTP.STATUS_OK));
     } else {
-      return res.status(HTTP.STATUS_NOT_FOUND).json(errorResponse('User not found', null, HTTP.STATUS_NOT_FOUND));
+      return res.status(HTTP.STATUS_NOT_FOUND).json(errorResponse(MESSAGE.USER_NOT_FOUND, null, HTTP.STATUS_NOT_FOUND));
     }
   } catch (err) {
-    return res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse('An error occurred', err.message, HTTP.STATUS_SERVER_ERROR));
+    return res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse(MESSAGE.GENERIC_ERROR, err.message, HTTP.STATUS_SERVER_ERROR));
   }
 };
 /**
@@ -45,12 +46,12 @@ exports.show = async (req, res) => {
 exports.create = async (req, res) => {
     try {
       const newUser = await userService.createUser(req.body);
-      return res.status(HTTP.STATUS_CREATED).json(successResponse('Users created successfully', newUser, HTTP.STATUS_CREATED));
+      return res.status(HTTP.STATUS_CREATED).json(successResponse(MESSAGE.USER_CREATED_SUCCESS, newUser, HTTP.STATUS_CREATED));
     } catch (err) {
       if (err.name === 'MongoError' && err.code === 11000) 
-        return res.status(HTTP.STATUS_CONFLICT).json(errorResponse('Email already exists', null, HTTP.STATUS_CONFLICT));
+        return res.status(HTTP.STATUS_CONFLICT).json(errorResponse(MESSAGE.EMAIL_EXIST, null, HTTP.STATUS_CONFLICT));
       
-      return res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse('An error occurred', err.message, HTTP.STATUS_SERVER_ERROR));
+      return res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse(MESSAGE.GENERIC_ERROR, err.message, HTTP.STATUS_SERVER_ERROR));
     }
 };
 /**
@@ -66,12 +67,12 @@ exports.updateUser = async (req, res) => {
     const updatedUser = await userService.updateUser(userId, userData);
     
     if (!updatedUser) 
-      return res.status(HTTP.STATUS_NOT_FOUND).json(errorResponse('User not found', null, HTTP.STATUS_NOT_FOUND));
+      return res.status(HTTP.STATUS_NOT_FOUND).json(errorResponse(MESSAGE.USER_NOT_FOUND, null, HTTP.STATUS_NOT_FOUND));
     
-    return res.status(HTTP.STATUS_OK).json(successResponse('User updated successfully', updatedUser, HTTP.STATUS_OK));
+    return res.status(HTTP.STATUS_OK).json(successResponse(MESSAGE.USER_UPDATED_SUCCESS, updatedUser, HTTP.STATUS_OK));
 
   } catch (error) {
-    return res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse('An error occurred', error.message, HTTP.STATUS_SERVER_ERROR));
+    return res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse(MESSAGE.GENERIC_ERROR, error.message, HTTP.STATUS_SERVER_ERROR));
   }
 };
 /**
@@ -86,12 +87,12 @@ exports.deleteUser = async (req, res) => {
     const deletedUser = await userService.deleteUser(userId);
     
     if (!deletedUser) 
-      return res.status(HTTP.STATUS_NOT_FOUND).json(errorResponse('User not found', null, HTTP.STATUS_NOT_FOUND));
+      return res.status(HTTP.STATUS_NOT_FOUND).json(errorResponse(MESSAGE.USER_NOT_FOUND, null, HTTP.STATUS_NOT_FOUND));
     
-    return res.status(HTTP.STATUS_REQUEST_ACCEPTED).json(successResponse('User deleted successfully', null, HTTP.STATUS_REQUEST_ACCEPTED));
+    return res.status(HTTP.STATUS_REQUEST_ACCEPTED).json(successResponse(MESSAGE.USER_DELETED_SUCCESS, null, HTTP.STATUS_REQUEST_ACCEPTED));
 
   } catch (error) {
-    return  res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse('An error occurred', error.message, HTTP.STATUS_SERVER_ERROR));
+    return  res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse(MESSAGE.GENERIC_ERROR, error.message, HTTP.STATUS_SERVER_ERROR));
   }
 };
 /**
@@ -102,9 +103,9 @@ exports.deleteUser = async (req, res) => {
 exports.login = async (req, res) => {
     try {
       const user = await userService.checkUserCredentials(req.body.email, req.body.password);
-      return res.status(HTTP.STATUS_CREATED).json(successResponse('Users logged in successfully', user, HTTP.STATUS_CREATED));
+      return res.status(HTTP.STATUS_CREATED).json(successResponse(MESSAGE.LOGIN_SUCCESS, user, HTTP.STATUS_CREATED));
     } catch (err) {
-      return res.status(HTTP.STATUS_BAD_REQUEST).json(errorResponse('An error occurred', err.message, HTTP.STATUS_BAD_REQUEST));
+      return res.status(HTTP.STATUS_BAD_REQUEST).json(errorResponse(MESSAGE.GENERIC_ERROR, err.message, HTTP.STATUS_BAD_REQUEST));
     }
 };
 /**
@@ -118,6 +119,6 @@ exports.sampleLogMessage = async (req, res) => {
     return res.status(HTTP.STATUS_OK).json(successResponse('Message logged in successfully', null, HTTP.STATUS_OK))
 
   } catch (error) {
-    return res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse('An error occurred', error.message, HTTP.STATUS_SERVER_ERROR))
+    return res.status(HTTP.STATUS_SERVER_ERROR).json(errorResponse(MESSAGE.GENERIC_ERROR, error.message, HTTP.STATUS_SERVER_ERROR))
   }
 }
